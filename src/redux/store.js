2c8cc2b9193bdd2d49bf817e43bc28089bd18c58
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-02 09:41:14 +0600

    feat: connect with redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { usersApi } from './api/usersApi';
+import usersReducer from './features/usersSlice';
 
 export const store = configureStore({
-  reducer: {},
-});
-
+    reducer: {
+      [usersApi.reducerPath]:usersApi.reducer,
+      users: usersReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(usersApi.middleware),
+  });
